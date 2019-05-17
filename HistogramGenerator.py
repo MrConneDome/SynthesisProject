9d04798458241719161401e5a82536f1957e4d7c
MrConneDome <49830964+MrConneDome@users.noreply.github.com>
2019-05-17 20:34:30 +0200

    Delete HistogramGenerator.py

--- HistogramGenerator.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Wed May 15 19:28:32 2019
-
-@author: Konrad Jarocki
-"""
-name = input("Gimme your name\n")
-print("hi, ", name,", nice to see you!")
-print("test test test")
