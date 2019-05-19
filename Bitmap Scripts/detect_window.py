0a95fc190526b42c7c95346e0e2d5584e976de1b
MrConneDome <konrad.jarocki@gmail.com>
2019-05-19 20:55:06 +0200

    Update detect_window.py

--- Bitmap Scripts/detect_window.py
+++ Bitmap Scripts/detect_window.py
@@ -8,21 +8,41 @@ import cv2
 import numpy as np
 
 
-def detectrectange(fn):
-    
+def detectrectange(fn,size_factor_min,size_factor_max):
+    #Opening the image
     img = cv2.imread(fn,0)
-          
-    ret,thresh = cv2.threshold(img,150,255,cv2.THRESH_BINARY)
-    img = cv2.cvtColor(img, cv2.COLOR_GRAY2RGB)   
+    height, width = img.shape[:2]
+    
+    #size of the windows for plot
+    cv2.namedWindow('Windows Detection', cv2.WINDOW_NORMAL)
+    cv2.resizeWindow('Windows Detection', 1000,1000)
+    
+    #Creating the threshold array for contour detection
+    ret,thresh = cv2.threshold(img,150,255,cv2.THRESH_BINARY) 
+    #changing image to RGB to plot in colors
+    img = cv2.cvtColor(img, cv2.COLOR_GRAY2RGB)      
     contours,hierarchy = cv2.findContours(thresh, 1, 2)
-
-    for cnt in contours:
+    
+    #Iterating every contour
+    for cnt in contours: 
         
-        image2 = cv2.drawContours(img, [cnt],-1, (0,50,50) ,2)     
+        #Finding parameters of bounding box 
         x,y,w,h = cv2.boundingRect(cnt)
-        image = cv2.rectangle(img,(x,y),(x+w,y+h),(110,0,50),2)
+        #Ratio compare to the whole facade
+        ratio = round(int(w*h)/int(height*width),4)
+        #Position for text
+        M = cv2.moments(cnt)
+        cX = int((M["m10"] / M["m00"]))
+        cY = int((M["m01"] / M["m00"]))
         
-        cv2.imshow('sample image',image)
-        cv2.waitKey(0)
+        #Condtion for too small and too big windows
+        if w*h<size_factor_max*height*width and w*h>size_factor_min*height*width:
+            #Plotting the results
+            image = cv2.rectangle(img,(x,y),(x+w,y+h),(110,0,50),2)
+            cv2.putText(image, 'R:'+str(ratio), (cX, cY), cv2.FONT_HERSHEY_SIMPLEX, 0.3, (0, 120, 255), 1)
+            cv2.imshow('Windows Detection',image)
+        else:
+            print('too big or too small')
+    cv2.waitKey(0)
         
-detectrectange('test.jpg')
+detectrectange('test2.jpg',0.009,0.4)
