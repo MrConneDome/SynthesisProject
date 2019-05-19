55b125e4b1179768da78c846548b483fc3a7810f
MrConneDome <konrad.jarocki@gmail.com>
2019-05-19 20:00:06 +0200

    Added threshold

--- Bitmap Scripts/filters.py
+++ Bitmap Scripts/filters.py
@@ -7,7 +7,7 @@ from scipy.ndimage import gaussian_filter
 from scipy.signal import medfilt
 import numpy as np
 
-def applygaussian(fn, sigma):
+def applygaussian(fn, sigma,threshold):
     #applying gaussian filter with given sigma on the numpy table imported from text file
     
     raster = np.loadtxt(fn) 
@@ -15,7 +15,18 @@ def applygaussian(fn, sigma):
     print(raster)
     raster_after = gaussian_filter(raster, sigma)
     print(raster_after)
-    
+    raster_threshold = []
+    for record in raster_after:
+        for i in record:
+            
+            if i<threshold:
+                
+                raster_threshold.append(0.0)
+                
+            else:
+                raster_threshold.append(255.0)
+    raster_after = np.array(raster_threshold)
+    raster_after = raster_after.reshape((49,49))
     return raster_after
 
 def applymedian(fn,kernel_size):
